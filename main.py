426617a73da302fab9e736ca56a92715de7f894f
abdulrehman <abdulrehman.workmail@gmail.com>
2022-12-29 11:42:27 +0500

    Checking remote.

--- main.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch.nn import Linear
-import torch.nn.functional as F
-from torch.optim import Adam
-
-from torch_geometric.data import DataLoader
-from torch_geometric.nn import RGCNConv
-from torch_geometric.utils import dropout_adj
